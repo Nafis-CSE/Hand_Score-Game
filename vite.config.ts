658704fd9df02081e5eeee73d9a12a8fa01ff5d8
Shahariyar Nafis <nafis@example.com>
2025-08-08 23:12:14 +0600

    added live site

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Hand_Score-game/',
+  base: '/Hand_Score-Game/',
   plugins: [react()],
 })
